acecd9211259098746b429fa1db11e8f9e9e4f06
Michael Goboola <raniahgobz@outlook.com>
2023-01-31 12:00:37 +0000

    cleared bug in the jinja extensions

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
 from flask import Flask, render_template
 
-app = Flask("__name__")
 
-@app.route("/):
-def index():
-    render_template("/templates/0-index.html")
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / 
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-   app.run()
-
+    app.run(port="8000", host="0.0.0.0", debug=True)
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,10 +1,32 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
 
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
 
 
-class Config:
-    LANGUAGES = ["en", "fr"]
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / 
+    """
+    return render_template('1-index.html')
 
+
+if __name__ == "__main__":
+    app.run(port="8000", host="0.0.0.0", debug=True)
